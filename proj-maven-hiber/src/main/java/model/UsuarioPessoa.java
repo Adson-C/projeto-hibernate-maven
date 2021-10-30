7f380fb5a5464cc6ba3921c0816fc21ed2df3426
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-10-30 00:32:58 -0300

    projeto criado

---
+++ proj-maven-hiber/src/main/java/model/UsuarioPessoa.java
@@ -0,0 +1,65 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UsuarioPessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private String sobreNome;
+	private String email;
+	private String login;
+	private String senha;
+	private int idade;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+}
---
+++ proj-maven-hiber/src/test/java/projmavenhiber/TesteHibernate.java
@@ -0,0 +1,12 @@
+package projmavenhiber;
+
+import org.junit.Test;
+
+public class TesteHibernate {
+	
+	@Test
+	public void testeHibernateUtil() {
+		HibernateUtil.getEntityManager();
+	}
+
+}
