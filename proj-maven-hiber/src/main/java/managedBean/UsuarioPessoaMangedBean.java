0f1c221610edb78fd99104f3999e926b57fd9b42
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-09-01 22:35:37 -0300

    Criando formulário

---
+++ proj-maven-hiber/src/main/java/managedBean/UsuarioPessoaMangedBean.java
@@ -0,0 +1,25 @@
+package managedBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
+@ManagedBean(name = "usuarioPessoaManagedBean")
+@ViewScoped
+public class UsuarioPessoaMangedBean {
+	
+	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
+	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+	
+	public UsuarioPessoa getUsuarioPessoa() {
+		return usuarioPessoa;
+	}
+	public void setUsuarioPessoa(UsuarioPessoa usuarioPessoa) {
+		this.usuarioPessoa = usuarioPessoa;
+	}
+	
+	
+
+}
