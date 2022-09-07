881dedca6d5db6d571ad69a1a48b059fb1507b52
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-09-07 13:34:28 -0300

    Instalando PrimeFaces

--- proj-maven-hiber/src/main/java/model/UsuarioPessoa.java
+++ proj-maven-hiber/src/main/java/model/UsuarioPessoa.java
@@ -7,8 +7,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 
