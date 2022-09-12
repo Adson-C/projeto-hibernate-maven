f8128fb1d8f50ec4546c107205b264b2eb67a530
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-09-11 23:42:46 -0300

    Consumindo WEB Service CEP Restfull em JSF finalizado

--- proj-maven-hiber/src/main/java/managedBean/UsuarioPessoaMangedBean.java
+++ proj-maven-hiber/src/main/java/managedBean/UsuarioPessoaMangedBean.java
@@ -1,5 +1,10 @@
 package managedBean;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +14,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
+
+import com.google.gson.Gson;
 
 import dao.Daousuario;
 import model.UsuarioPessoa;
@@ -30,7 +37,31 @@ public class UsuarioPessoaMangedBean {
 		
 		try {
 			
-			System.out.println("Cep digitado: " + usuarioPessoa.getCep());
+			
+			URL url = new URL("https://viacep.com.br/ws/"+usuarioPessoa.getCep()+"/json/");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+			
+			while ((cep = br.readLine()) != null) {
+				jsonCep.append(cep);
+			}
+			
+			UsuarioPessoa userCepPessoa = new Gson().fromJson(jsonCep.toString(), UsuarioPessoa.class);
+			
+			usuarioPessoa.setCep(userCepPessoa.getCep());
+			usuarioPessoa.setLogradouro(userCepPessoa.getLogradouro());
+			usuarioPessoa.setComplemento(userCepPessoa.getComplemento());
+			usuarioPessoa.setBairro(userCepPessoa.getBairro());
+			usuarioPessoa.setLocalidade(userCepPessoa.getLocalidade());
+			usuarioPessoa.setUf(userCepPessoa.getUf());
+			usuarioPessoa.setUndidade(userCepPessoa.getUndidade());
+			usuarioPessoa.setIbge(userCepPessoa.getIbge());
+			usuarioPessoa.setGia(userCepPessoa.getGia());
+			
 			
 			
 		} catch (Exception e) {
