e1a1451fe6b27dbab6e4f24f72c67950d8ced126
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-09-17 17:22:09 -0300

    Removendo emails e telefones em cascata

--- proj-maven-hiber/src/main/java/dao/Daousuario.java
+++ proj-maven-hiber/src/main/java/dao/Daousuario.java
@@ -7,11 +7,10 @@ public class Daousuario<E> extends DaoGeneric<UsuarioPessoa> {
 	public void removerUsuario(UsuarioPessoa pessoa) throws Exception {
 		getEntityManager().getTransaction().begin();
 
-		String sqlDeleleFone = "delete from telefoneuser where usuariopessoa_id  = " + pessoa.getId();
-		getEntityManager().createNativeQuery(sqlDeleleFone).executeUpdate();
+		getEntityManager().remove(pessoa);
+		
 		getEntityManager().getTransaction().commit();
 		
-		super.deletarPorId(pessoa);
 		
 	}
 	
